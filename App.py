c905a6679750cb29ea9bf3226bfa12280aae299a
pvpvfrvter99 <pvpvfrvter99@gmail.com>
2026-06-04 02:08:08 +0800

    Add files via upload

--- App.py
+++ App.py
@@ -270,4 +270,7 @@ if __name__ == '__main__':
             ]
             db.session.add_all(default_menus)
         db.session.commit()
-    app.run(debug=True)
+if __name__ == "__main__":
+                import os
+                port = int(os.environ.get("PORT", 5000))
+                app.run(host="0.0.0.0", port=port)
